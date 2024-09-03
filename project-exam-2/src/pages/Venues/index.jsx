b5a1541c5af540f1e7d29fff78601a9f57f43caa
absim29 <simon.bianca29@gmail.com>
2024-09-03 20:36:56 +0200

    Update index.jsx

--- project-exam-2/src/pages/Venues/index.jsx
+++ project-exam-2/src/pages/Venues/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import VenueCard from "../../components/VenueCard";
+import { useFetch } from "../../hooks/useFetch";
+
+const url = "https://v2.api.noroff.dev/holidaze/venues";
 
 function Venues() {
+  const { data, isError, isLoading } = useFetch(url);
+  if (isLoading) {
+    return <div>Loading venues...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading data</div>;
+  }
+
   return (
     <Layout>
-      <h2 className="second-font">Find your next holiday destination</h2>
-      <VenueCard />
+      <h2 className="second-font mt-2">Find your next holiday destination</h2>
+      <div className="d-flex flex-wrap justify-content-center gap-4 mx-5 px-5">
+        {data.map((venue) => (
+          <VenueCard key={venue.id} venue={venue} />
+        ))}
+      </div>
     </Layout>
   );
 }
