eb7d00781769ab8f323cdd1b6775674abd167620
absim29 <simon.bianca29@gmail.com>
2024-09-03 16:40:16 +0200

    Update index.jsx

--- project-exam-2/src/components/Layout/index.jsx
+++ project-exam-2/src/components/Layout/index.jsx
@@ -4,11 +4,13 @@ import Footer from "../Footer";
 
 function Layout({ children }) {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main>{children}</main>
+      <main className="flex-fill d-flex flex-column flex-wrap align-items-center">
+        {children}
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
