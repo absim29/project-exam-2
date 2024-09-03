f230be3bc7cc582d59f05c7d157fe34cbc0dd3ff
absim29 <simon.bianca29@gmail.com>
2024-09-03 13:59:49 +0200

    Update index.jsx

--- project-exam-2/src/components/Footer/index.jsx
+++ project-exam-2/src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Footer() {
-  <footer>
-    <p>All rights reserved. Copyright 2024 - HOLIDAZE</p>
-  </footer>;
+  return (
+    <footer>
+      <p>All rights reserved. Copyright 2024 - HOLIDAZE</p>
+    </footer>
+  );
 }
 
 export default Footer;
