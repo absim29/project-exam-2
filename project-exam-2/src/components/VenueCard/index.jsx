6e42483e7adc3659b3bd7fdf6d1aa307085b47d2
absim29 <simon.bianca29@gmail.com>
2024-09-03 17:33:23 +0200

    Venue card with data from the API

--- project-exam-2/src/components/VenueCard/index.jsx
+++ project-exam-2/src/components/VenueCard/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import venue from "../../assets/venue-card.jpg";
+import VenueImage from "../../assets/venue-card.jpg";
 import Rating from "@mui/material/Rating";
 
-function VenueCard() {
+function VenueCard({ venue }) {
+  // Extract relevant details from the venue data
+  const { name, media, price, rating, location } = venue;
+
   return (
     <>
       <div className="venue-card">
-        <img src={venue} alt="The Venue" className="card-image" />
-        <h2 className="first-font">Name, Place</h2>
-        <h4 className="first-font">Price:$100</h4>
-        <Rating name="size-small" defaultValue={2} size="small" />
+        <img
+          src={media.length > 0 && media[0].url ? media[0].url : VenueImage}
+          alt={media.length > 0 && media[0].alt ? media[0].alt : name}
+          className="card-image"
+        />
+        <h2 className="first-font">{name}</h2>
+        <h3 className="first-font">
+          {location.city}, {location.country}
+        </h3>
+        <h4 className="first-font">Price:${price}</h4>
+        <Rating name="size-small" value={rating || 0} size="small" readOnly />
       </div>
     </>
   );
