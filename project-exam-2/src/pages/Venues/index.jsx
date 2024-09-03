54029a8415891159774b5fa16397790095533522
absim29 <simon.bianca29@gmail.com>
2024-09-03 16:40:35 +0200

    Update index.jsx

--- project-exam-2/src/pages/Venues/index.jsx
+++ project-exam-2/src/pages/Venues/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import VenueCard from "../../components/VenueCard";
 
 function Venues() {
   return (
     <Layout>
-      <p>This is a text.</p>
+      <h2 className="second-font">Find your next holiday destination</h2>
+      <VenueCard />
     </Layout>
   );
 }
