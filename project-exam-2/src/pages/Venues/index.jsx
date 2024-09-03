c6360db1a2710ff9bd1322894a364e9faa09ff9d
absim29 <simon.bianca29@gmail.com>
2024-09-03 13:31:21 +0200

    Update index.jsx

---
+++ project-exam-2/src/pages/Venues/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+function Venues() {
+  return (
+    <Layout>
+      <p>This is a text.</p>
+    </Layout>
+  );
+}
+
+export default Venues;
