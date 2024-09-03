b7ef58fe9b976cca8749b469872b0c8d69c54341
absim29 <simon.bianca29@gmail.com>
2024-09-03 20:35:46 +0200

    updated venue card

--- project-exam-2/src/components/VenueCard/index.jsx
+++ project-exam-2/src/components/VenueCard/index.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import VenueImage from "../../assets/venue-card.jpg";
 import Rating from "@mui/material/Rating";
+import { Link } from "react-router-dom";
 
 function VenueCard({ venue }) {
   // Extract relevant details from the venue data
   const { name, media, price, rating, location } = venue;
 
+  const locationString = [
+    location.city || location.country
+      ? `${location.city || ""}${
+          location.city && location.country ? ", " : ""
+        }${location.country || ""}`
+      : "Location Unavailable",
+  ];
+
   return (
     <>
-      <div className="venue-card">
-        <img
-          src={media.length > 0 && media[0].url ? media[0].url : VenueImage}
-          alt={media.length > 0 && media[0].alt ? media[0].alt : name}
-          className="card-image"
-        />
-        <h2 className="first-font">{name}</h2>
-        <h3 className="first-font">
-          {location.city}, {location.country}
-        </h3>
-        <h4 className="first-font">Price:${price}</h4>
-        <Rating name="size-small" value={rating || 0} size="small" readOnly />
-      </div>
+      <Link
+        to={`/venues/${venue.id}`}
+        className="text-decoration-none text-black"
+      >
+        <div className="venue-card">
+          <img
+            src={media.length > 0 && media[0].url ? media[0].url : VenueImage}
+            alt={media.length > 0 && media[0].alt ? media[0].alt : name}
+            className="card-image"
+          />
+          <h2 className="first-font fs-5 mt-2">{name}</h2>
+          <h3 className="first-font fs-6">{locationString}</h3>
+          <h4 className="first-font fs-4">Price:${price}</h4>
+          <Rating name="size-small" value={rating || 0} size="small" readOnly />
+        </div>
+      </Link>
     </>
   );
 }
