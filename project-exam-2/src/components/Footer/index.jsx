234c9132aac5a3fae4bf6916d46bdb7ca2813820
absim29 <simon.bianca29@gmail.com>
2024-09-03 16:40:02 +0200

    Update index.jsx

--- project-exam-2/src/components/Footer/index.jsx
+++ project-exam-2/src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="d-flex justify-content-center">
       <p>All rights reserved. Copyright 2024 - HOLIDAZE</p>
     </footer>
   );
