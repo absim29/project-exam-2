943d544eaa2371b3b0d416f37fbb00a9eb2ae4b7
absim29 <simon.bianca29@gmail.com>
2024-09-03 13:31:27 +0200

    Update App.jsx

--- project-exam-2/src/App.jsx
+++ project-exam-2/src/App.jsx
@@ -1,11 +1,35 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Venues from "./pages/Venues";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        {/* The LandingPage is displayed when the user is at the root URL */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/venues" element={<Venues />} />
+        {/* Page not found handler for unmatched routes */}
+        <Route
+          path="*"
+          element={
+            <>
+              <h1>Page not found</h1>
+              <div className="prodContainer">
+                <p>
+                  We're sorry, but the page you are looking for doesn't exist.
+                </p>
+                <p>
+                  It might have been moved or deleted, or perhaps the URL is
+                  incorrect.
+                </p>
+              </div>
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
