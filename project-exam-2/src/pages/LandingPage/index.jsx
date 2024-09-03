07a4cdf3dfae26ab9435251a472edc1c32c52540
absim29 <simon.bianca29@gmail.com>
2024-09-03 11:39:53 +0200

    Update index.jsx

--- project-exam-2/src/pages/LandingPage/index.jsx
+++ project-exam-2/src/pages/LandingPage/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import BackgroundVideo from "../../components/BackgroundVideo";
 import MyButton from "../../components/Button";
+import Logo from "../../components/Logo";
 
 function LandingPage() {
   return (
     <>
       <BackgroundVideo />
-      <div className="landing-text">
-        <h1>Escape the ordinary</h1>
-        <h2>Your adventure starts here</h2>
+      <Logo />
+      <div className="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-uppercase first-font">Escape the ordinary</h1>
+        <h2 className="second-font">Your Next Adventure Starts Here</h2>
         <MyButton />
+        <h3 className="first-font fs-5 mt-3">Need an account? SIGN UP</h3>
+        <div className="third-font d-flex flex-column justify-content-center align-items-center mx-5 px-5">
+          <p>
+            By signing in or creating an account, you agree with our{" "}
+            <u>Terms & conditions</u> and <u>Privacy statement</u>
+          </p>
+          <p>All rights reserved. Copyright 2024 - HOLIDAZE</p>
+        </div>
       </div>
     </>
   );
