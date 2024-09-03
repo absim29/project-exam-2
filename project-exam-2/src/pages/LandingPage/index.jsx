8561d87eaf2d02f53a9513f344647b79cda729ce
absim29 <simon.bianca29@gmail.com>
2024-09-03 13:31:18 +0200

    Update index.jsx

--- project-exam-2/src/pages/LandingPage/index.jsx
+++ project-exam-2/src/pages/LandingPage/index.jsx
@@ -11,7 +11,7 @@ function LandingPage() {
       <div className="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-center align-items-center">
         <h1 className="text-uppercase first-font">Escape the ordinary</h1>
         <h2 className="second-font">Your Next Adventure Starts Here</h2>
-        <MyButton />
+        <MyButton to="/venues" label="Login" />
         <h3 className="first-font fs-5 mt-3">Need an account? SIGN UP</h3>
         <div className="third-font d-flex flex-column justify-content-center align-items-center mx-5 px-5">
           <p>
