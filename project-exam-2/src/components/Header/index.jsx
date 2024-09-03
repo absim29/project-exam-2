b0b959167683055d5d6fcfdcddfa6209846f28fd
absim29 <simon.bianca29@gmail.com>
2024-09-03 14:46:31 +0200

    Updated header with styling

--- project-exam-2/src/components/Header/index.jsx
+++ project-exam-2/src/components/Header/index.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import Logo from "../Logo";
 import { NavLink } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
 
 function Header() {
+  const headerBackground = {
+    backgroundColor: "#80A3AB",
+    paddingBottom: "10px",
+  };
+
   return (
-    <>
+    <div style={headerBackground}>
       <Logo />
       <nav>
-        <ul>
-          <li>
-            <NavLink to="/venues">Venues</NavLink>{" "}
-          </li>
-          <li>
-            Username{" "}
-            <ul>
-              <li>My Profile</li>
-              <li>My Bookings</li>
-              <li>Favourites</li>
-              <li>Manage Bookings</li>
-              <li>Sign Out</li>
-            </ul>
-          </li>
+        <ul className="d-flex justify-content-end gap-3 pt-3 px-2">
+          <NavLink to="/venues" className="text-decoration-none text-black">
+            Venues
+          </NavLink>{" "}
+          <NavDropdown title="Username" menuVariant="dark">
+            <NavDropdown.Item as={NavLink} to="/profile">
+              My Profile
+            </NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/bookings">
+              My Bookings
+            </NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/favourites">
+              Favourites
+            </NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/manage-bookings">
+              Manage Bookings
+            </NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/signout">
+              Sign Out
+            </NavDropdown.Item>
+          </NavDropdown>
         </ul>
       </nav>
-    </>
+    </div>
   );
 }
 
