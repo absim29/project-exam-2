f8697fcfd0cd4ed471c1693cd5201d484882710e
absim29 <simon.bianca29@gmail.com>
2024-09-04 14:15:49 +0200

    Update index.jsx

--- project-exam-2/src/components/Header/index.jsx
+++ project-exam-2/src/components/Header/index.jsx
@@ -1,42 +1,45 @@
 import React from "react";
 import Logo from "../Logo";
 import { NavLink } from "react-router-dom";
-import { NavDropdown } from "react-bootstrap";
+import { Navbar, NavDropdown } from "react-bootstrap";
 
 function Header() {
   const headerBackground = {
     backgroundColor: "#80A3AB",
     paddingBottom: "10px",
+    paddingTop: "0",
   };
 
   return (
-    <div style={headerBackground}>
-      <Logo />
-      <nav>
-        <ul className="d-flex justify-content-end gap-3 pt-3 px-2">
-          <NavLink to="/venues" className="text-decoration-none text-black">
-            Venues
-          </NavLink>{" "}
-          <NavDropdown title="Username" menuVariant="dark">
-            <NavDropdown.Item as={NavLink} to="/profile">
-              My Profile
-            </NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/bookings">
-              My Bookings
-            </NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/favourites">
-              Favourites
-            </NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/manage-bookings">
-              Manage Bookings
-            </NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/signout">
-              Sign Out
-            </NavDropdown.Item>
-          </NavDropdown>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <Navbar sticky="top" className="pt-0">
+        <div style={headerBackground} className="w-100">
+          <Logo />
+          <ul className="d-flex justify-content-end gap-3 pt-3 px-2">
+            <NavLink to="/venues" className="text-decoration-none text-black">
+              Venues
+            </NavLink>{" "}
+            <NavDropdown title="Username" menuVariant="light" drop="start">
+              <NavDropdown.Item as={NavLink} to="/profile">
+                My Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/bookings">
+                My Bookings
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/favourites">
+                Favourites
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/manage-bookings">
+                Manage Bookings
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/signout">
+                Sign Out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </ul>
+        </div>
+      </Navbar>
+    </>
   );
 }
 
