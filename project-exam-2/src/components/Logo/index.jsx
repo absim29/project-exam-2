b4f8d2b3bc57ca1bfcc5a85720ca72a674359d1e
absim29 <simon.bianca29@gmail.com>
2024-09-04 14:15:53 +0200

    Update index.jsx

--- project-exam-2/src/components/Logo/index.jsx
+++ project-exam-2/src/components/Logo/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../assets/Holidaze-2.png";
+import { Link } from "react-router-dom";
 
 function Logo() {
   const logoStyle = {
@@ -9,7 +10,9 @@ function Logo() {
   };
   return (
     <div className="position-absolute top-0 start-0">
-      <img src={logo} alt="Holidaze Logo" style={logoStyle} />
+      <Link to={"/"}>
+        <img src={logo} alt="Holidaze Logo" style={logoStyle} />
+      </Link>
     </div>
   );
 }
