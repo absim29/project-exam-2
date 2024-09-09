469cccd99728ccc6c1e4c930a5e3a3d5de42cb35
absim29 <simon.bianca29@gmail.com>
2024-09-09 15:34:39 +0200

    Added loading spinner

--- project-exam-2/src/pages/SingleVenue/index.jsx
+++ project-exam-2/src/pages/SingleVenue/index.jsx
@@ -3,29 +3,27 @@ import Layout from "../../components/Layout";
 import { useParams } from "react-router-dom";
 import VenuePage from "../../components/VenuePage";
 import { useFetchSingle } from "../../hooks/useFetchSingle";
+import { Spinner } from "react-bootstrap";
 
 const url = "https://v2.api.noroff.dev/holidaze/venues";
 
 function SingleVenue() {
   let { id } = useParams();
   const { data, isError, isLoading } = useFetchSingle(url, id);
-  if (isLoading) {
-    return <div>Loading venue details...</div>;
-  }
-
-  if (isError) {
-    return <div>Error loading venue details. Please try again later.</div>;
-  }
-
-  if (!data) {
-    return (
-      <div>No venue data found. Please check the ID or try again later.</div>
-    );
-  }
 
   return (
     <Layout>
-      <VenuePage key={data.id} venue={data} />
+      {isLoading ? (
+        <Spinner animation="border" role="status" className="text-black">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : isError ? (
+        <div>Error loading data</div>
+      ) : !data ? (
+        <div>No venue data found. Please check the ID or try again later.</div>
+      ) : (
+        <VenuePage key={data.id} venue={data} />
+      )}
     </Layout>
   );
 }
--- project-exam-2/src/pages/Venues/index.jsx
+++ project-exam-2/src/pages/Venues/index.jsx
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout";
 import VenueCard from "../../components/VenueCard";
 import { useFetch } from "../../hooks/useFetch";
 import MyButton from "../../components/Button";
+import { Spinner } from "react-bootstrap";
 
 const url = (page, pageSize) =>
   `https://v2.api.noroff.dev/holidaze/venues?limit=${pageSize}&page=${page}`;
@@ -11,39 +12,40 @@ function Venues() {
   const { data, isError, isLoading, page, prevPage, nextPage, hasMoreData } =
     useFetch(url);
 
-  if (isLoading) {
-    return <div>Loading venues...</div>;
-  }
-
-  if (isError) {
-    return <div>Error loading data</div>;
-  }
-
   return (
     <Layout>
       <h2 className="second-font mt-2">Find your next holiday destination</h2>
-
-      {data.length === 0 ? (
-        <div>No more venues available at this time.</div>
+      {isLoading ? (
+        <Spinner animation="border" role="status" className="text-black">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : isError ? (
+        <div>Error loading data</div>
       ) : (
-        <div className="d-flex flex-wrap justify-content-center gap-4 mx-5 px-5">
-          {data.map((venue) => (
-            <VenueCard key={venue.id} venue={venue} />
-          ))}
-        </div>
+        <>
+          {data.length === 0 ? (
+            <div>No more venues available at this time.</div>
+          ) : (
+            <div className="d-flex flex-wrap justify-content-center gap-4 mx-5 px-5">
+              {data.map((venue) => (
+                <VenueCard key={venue.id} venue={venue} />
+              ))}
+            </div>
+          )}
+          <div className="d-flex w-50 pb-5 justify-content-between">
+            <MyButton
+              disabled={isLoading || page === 1} // Disable previous button if on the first page
+              onClick={prevPage}
+              label="<"
+            />
+            <MyButton
+              disabled={!hasMoreData} // Disable next button if no data is returned
+              onClick={nextPage}
+              label=">"
+            />
+          </div>
+        </>
       )}
-      <div className="d-flex w-50 pb-5 justify-content-between">
-        <MyButton
-          disabled={isLoading || page === 1} // Disable previous button if on the first page
-          onClick={prevPage}
-          label="<"
-        />
-        <MyButton
-          disabled={!hasMoreData} // Disable next button if no data is returned
-          onClick={nextPage}
-          label=">"
-        />
-      </div>
     </Layout>
   );
 }
