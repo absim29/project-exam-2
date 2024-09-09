6f25cc8b5328ec958c2fd3a552df372c3eb5a068
absim29 <simon.bianca29@gmail.com>
2024-09-09 14:05:29 +0200

    Update index.jsx

--- project-exam-2/src/pages/LandingPage/index.jsx
+++ project-exam-2/src/pages/LandingPage/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BackgroundVideo from "../../components/BackgroundVideo";
 import MyButton from "../../components/Button";
 import Logo from "../../components/Logo";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
@@ -11,7 +12,9 @@ function LandingPage() {
       <div className="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-center align-items-center">
         <h1 className="text-uppercase first-font">Escape the ordinary</h1>
         <h2 className="second-font">Your Next Adventure Starts Here</h2>
-        <MyButton to="/venues" label="Explore" />
+        <Link to="/venues">
+          <MyButton label="Explore" />
+        </Link>
         <h3 className="first-font fs-5 mt-3">Already a user? LOGIN</h3>
         <div className="third-font d-flex flex-column justify-content-center align-items-center mx-5 px-5">
           <p>
