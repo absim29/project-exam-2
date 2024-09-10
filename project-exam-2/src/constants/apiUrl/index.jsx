c973e02a55e560cbb1a8d3e0421dcd53d2f2e96e
absim29 <simon.bianca29@gmail.com>
2024-09-10 23:22:56 +0200

    Update index.jsx

--- project-exam-2/src/constants/apiUrl/index.jsx
+++ project-exam-2/src/constants/apiUrl/index.jsx
@@ -5,3 +5,7 @@ export const VENUES_API = "/holidaze/venues";
 export const BOOKINGS_API = "/holidaze/bookings";
 export const PROFILES_API = "/holidaze/profiles";
 export const VENUE_MANAGER = "?_holidaze=true";
+
+export const API_KEY_AUTH = "/auth/create-api-key";
+export const API_KEY = "23bd9a9e-044a-4859-a711-2172c0f92186";
+export const API_KEY_2 = "64dbcd87-e0e1-4046-8b6c-b67f5ab92c7a";
