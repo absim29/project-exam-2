ddaa43105bc24c8f7f1ca55a9637031dcc903f5c
absim29 <simon.bianca29@gmail.com>
2024-09-10 19:21:08 +0200

    all APIs

---
+++ project-exam-2/src/constants/apiUrl/index.jsx
@@ -0,0 +1,6 @@
+export const BASE_API = "https://v2.api.noroff.dev";
+export const REGISTER_API = "/auth/register";
+export const LOGIN_API = "/auth/login";
+export const VENUES_API = "/holidaze/venues";
+export const BOOKINGS_API = "/holidaze/bookings";
+export const PROFILES_API = "/holidaze/profiles";
