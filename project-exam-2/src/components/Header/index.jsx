c9602164b4fea8448020b04fcc7bbc0f8bb0d61f
absim29 <simon.bianca29@gmail.com>
2024-09-10 15:34:58 +0200

    Added login and registration to Navbar

--- project-exam-2/src/components/Header/index.jsx
+++ project-exam-2/src/components/Header/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Logo";
 import { NavLink } from "react-router-dom";
 import { Navbar, NavDropdown } from "react-bootstrap";
+import LoginModal from "../LoginModal";
+import RegistrationModal from "../RegistrationModal";
 
 function Header() {
   const headerBackground = {
@@ -10,16 +12,39 @@ function Header() {
     paddingTop: "0",
   };
 
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showRegistrationModal, setShowRegistrationModal] = useState(false);
+
+  // Handlers for Login Modal
+  const handleShowLogin = () => setShowLoginModal(true);
+  const handleCloseLogin = () => setShowLoginModal(false);
+
+  // Handlers for Registration Modal
+  const handleShowRegistration = () => setShowRegistrationModal(true);
+  const handleCloseRegistration = () => setShowRegistrationModal(false);
+
   return (
     <>
-      <Navbar sticky="top" className="pt-0">
+      <Navbar sticky="top" className="p-0">
         <div style={headerBackground} className="w-100">
           <Logo />
           <ul className="d-flex justify-content-end gap-3 pt-3 px-2">
             <NavLink to="/venues" className="text-decoration-none text-black">
               Venues
             </NavLink>{" "}
-            <NavDropdown title="Username" menuVariant="light" drop="start">
+            <NavLink
+              onClick={handleShowLogin}
+              className="text-decoration-none text-black"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              onClick={handleShowRegistration}
+              className="text-decoration-none text-black"
+            >
+              Register
+            </NavLink>
+            {/* <NavDropdown title="Username" menuVariant="light" drop="start">
               <NavDropdown.Item as={NavLink} to="/profile">
                 My Profile
               </NavDropdown.Item>
@@ -35,10 +60,15 @@ function Header() {
               <NavDropdown.Item as={NavLink} to="/signout">
                 Sign Out
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </ul>
         </div>
       </Navbar>
+      <LoginModal show={showLoginModal} handleClose={handleCloseLogin} />
+      <RegistrationModal
+        show={showRegistrationModal}
+        handleClose={handleCloseRegistration}
+      />
     </>
   );
 }
