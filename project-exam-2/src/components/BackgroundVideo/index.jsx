91a31437afcad5cc79603738391ece8123d8d9a9
absim29 <simon.bianca29@gmail.com>
2024-09-16 12:23:32 +0200

    Update index.jsx

--- project-exam-2/src/components/BackgroundVideo/index.jsx
+++ project-exam-2/src/components/BackgroundVideo/index.jsx
@@ -3,8 +3,15 @@ import video from "../../assets/tropical-video.mp4";
 
 function BackgroundVideo() {
   return (
-    <div className="video-container">
-      <video src={video} autoPlay loop muted />
+    <div className="w-100 vh-100">
+      <video
+        style={{ objectFit: "cover" }}
+        src={video}
+        autoPlay
+        loop
+        muted
+        className="w-100 h-100"
+      />
     </div>
   );
 }
