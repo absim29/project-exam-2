92be09b8ba23bbe5f294bee6c4cdaefbfd16b1b1
absim29 <simon.bianca29@gmail.com>
2024-09-17 14:32:53 +0200

    Created a Modals folder to store all the modals
    Login modal
    Registration modal
    Avatar change modal

---
+++ project-exam-2/src/components/Modals/LoginModal/index.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { BASE_API, LOGIN_API, VENUE_MANAGER } from "../../../constants/apiUrl";
+import useAuthForm from "../../../hooks/useAuthForm";
+import { validateForm } from "../../../functions/FormValidation";
+import MyButton from "../../Button";
+
+const url = BASE_API + LOGIN_API + VENUE_MANAGER;
+
+function LoginModal({ show, handleClose }) {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  const {
+    userData,
+    setUserData,
+    loading,
+    error,
+    validationErrors,
+    handleSubmit,
+  } = useAuthForm(initialState, validateForm, url, handleClose);
+
+  return (
+    <Modal show={show} onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title className="first-font">WELCOME BACK</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>EMAIL</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="required"
+              value={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+            {validationErrors.email && (
+              <p style={{ color: "red" }}>{validationErrors.email}</p>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>PASSWORD</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="required"
+              value={userData.password}
+              onChange={(e) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+            {validationErrors.password && (
+              <p style={{ color: "red" }}>{validationErrors.password}</p>
+            )}
+          </Form.Group>
+          <MyButton type="submit" label={loading ? "Logging in..." : "LOGIN"} />
+
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </Form>
+        <div className="d-flex justify-content-center">
+          <p>-</p>
+          <p>OR</p>
+          <p>-</p>
+        </div>
+        <div className="d-flex justify-content-center">
+          <GoogleIcon fontSize="large" />
+          <FacebookIcon fontSize="large" />
+          <LinkedInIcon fontSize="large" />
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default LoginModal;
---
+++ project-exam-2/src/components/Modals/RegistrationModal/index.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { BASE_API, REGISTER_API } from "../../../constants/apiUrl";
+import useAuthForm from "../../../hooks/useAuthForm";
+import { validateForm } from "../../../functions/FormValidation";
+import MyButton from "../../Button";
+
+const url = BASE_API + REGISTER_API;
+
+function RegistrationModal({ show, handleClose }) {
+  const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    // avatar: { url: "" },
+    bio: "",
+    venueManager: false,
+  };
+  const {
+    userData,
+    setUserData,
+    loading,
+    error,
+    validationErrors,
+    handleSubmit,
+  } = useAuthForm(initialState, validateForm, url, handleClose);
+
+  // const handleAvatarChange = (e) => {
+  //   setUserData((prevData) => ({
+  //     ...prevData,
+  //     avatar: { url: e.target.value }, // Ensure avatar is an object with url property
+  //   }));
+  // };
+
+  return (
+    <Modal show={show} onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title className="first-font">REGISTRATION</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>NAME</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="required"
+              value={userData.name}
+              onChange={(e) =>
+                setUserData({ ...userData, name: e.target.value })
+              }
+            />
+            {validationErrors.name && (
+              <p style={{ color: "red" }}>{validationErrors.name}</p>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>EMAIL</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="required"
+              value={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+            {validationErrors.email && (
+              <p style={{ color: "red" }}>{validationErrors.email}</p>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>PASSWORD</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="required"
+              value={userData.password}
+              onChange={(e) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+            {validationErrors.password && (
+              <p style={{ color: "red" }}>{validationErrors.password}</p>
+            )}
+          </Form.Group>
+          {/* <Form.Group className="mb-3" controlId="formBasicAvatar">
+            <Form.Label>AVATAR</Form.Label>
+            <Form.Control
+              type="url"
+              name="avatarUrl"
+              placeholder="optional"
+              value={userData.avatar.url}
+              onChange={handleAvatarChange}
+            />
+            {validationErrors.avatar && (
+              <p style={{ color: "red" }}>{validationErrors.avatar}</p>
+            )}
+          </Form.Group> */}
+          <Form.Group className="mb-3" controlId="formBasicBio">
+            <Form.Label>BIO</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="optional"
+              value={userData.bio}
+              onChange={(e) =>
+                setUserData({ ...userData, bio: e.target.value })
+              }
+            />
+            {validationErrors.bio && (
+              <p style={{ color: "red" }}>{validationErrors.bio}</p>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              type="checkbox"
+              label="REGISTER AS A VENUE MANAGER"
+              value={userData.venueManager}
+              onChange={(e) =>
+                setUserData({ ...userData, venueManager: e.target.checked })
+              }
+            />
+          </Form.Group>
+          <MyButton
+            type="submit"
+            label={loading ? "Registering..." : "REGISTER"}
+          />
+
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </Form>
+        <div className="d-flex justify-content-center">
+          <p>-</p>
+          <p>OR</p>
+          <p>-</p>
+        </div>
+        <div className="d-flex justify-content-center">
+          <GoogleIcon fontSize="large" />
+          <FacebookIcon fontSize="large" />
+          <LinkedInIcon fontSize="large" />
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default RegistrationModal;
