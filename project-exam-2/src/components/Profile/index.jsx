68491f5ae3220c4fa502db1923eddcd4a039d40c
absim29 <simon.bianca29@gmail.com>
2024-09-17 18:09:11 +0200

    Update profile page

--- project-exam-2/src/components/Profile/index.jsx
+++ project-exam-2/src/components/Profile/index.jsx
@@ -1,36 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import UserImage from "../../assets/user-image.jpg";
+import MyButton from "../Button";
+import VenueCard from "../VenueCard";
+import AvatarModal from "../Modals/AvatarModal";
+import VenueManagerModal from "../Modals/VenueManagerModal";
+import AddVenueModal from "../Modals/AddVenueModal";
 
 function Profile({ user }) {
-  const { name, email, avatar, bio, venueManager, venues, bookings, _count } =
-    user;
+  const [showEditProfileModal, setShowEditProfileModal] = useState(false);
+  const handleShowEditProfile = () => setShowEditProfileModal(true);
+  const handleCloseEditProfile = () => setShowEditProfileModal(false);
+
+  const [showAddVenueModal, setShowAddVenueModal] = useState(false);
+  const handleShowAddVenue = () => setShowAddVenueModal(true);
+  const handleCloseAddVenue = () => setShowAddVenueModal(false);
+
+  const [showVenueManagerModal, setShowVenueManagerModal] = useState(false);
+  const handleShowVenueManager = () => setShowVenueManagerModal(true);
+  const handleCloseVenueManager = () => setShowVenueManagerModal(false);
+
+  const { name, avatar, bio, venueManager, bookings, venues, _count } = user;
 
   return (
     <>
-      <div className="venue-card">
+      <div className="profile-card d-flex justify-content-between bg-white p-5 w-50 m-5 rounded gap-5 shadow">
         <img
           src={avatar.url.length > 0 && avatar.url ? avatar.url : UserImage}
           alt={avatar.url.length > 0 && avatar.alt ? avatar.alt : name}
-          className="card-image"
+          className="rounded-circle w-50"
+          onError={(e) => {
+            e.target.src = UserImage;
+          }}
         />
-        <h2 className="first-font fs-5 mt-2">{name}</h2>
-        <h3 className="first-font fs-6">{email}</h3>
-        <h4 className="first-font fs-4">{bio}</h4>
-        <h4 className="first-font fs-4">
-          Venue manager: {venueManager ? "Yes" : "No"}
-        </h4>
-        <h4 className="first-font fs-4">
-          {venues > 0
-            ? venues.join(", ") // Join array items into a string, separated by commas
-            : "No venues"}
-        </h4>
-        <h4 className="first-font fs-4">
-          {bookings > 0
-            ? bookings.join(", ") // Join array items into a string, separated by commas
-            : "No bookings"}
-        </h4>
-        <h4 className="first-font fs-4">Bookings: {_count.bookings}</h4>
-        <h4 className="first-font fs-4">Venues: {_count.venues}</h4>
+        <div className="d-flex flex-column justify-content-between">
+          <div>
+            <h4 className="first-font fs-4">{name}</h4>
+          </div>
+          <div>
+            <h4 className="first-font fs-4">
+              {venueManager ? "Venue manager: Yes" : "Venue manager: No"}
+            </h4>
+            <h4 className="first-font fs-4">Bookings: {_count.bookings}</h4>
+          </div>
+          <MyButton label="Edit Profile" onClick={handleShowEditProfile} />
+          <AvatarModal
+            show={showEditProfileModal}
+            handleClose={handleCloseEditProfile}
+            user={user}
+          />
+        </div>
+      </div>
+
+      <h4 className="second-font fs-1">Bio</h4>
+      <p className="third-font fs-4">{bio}</p>
+
+      <div className="d-flex gap-5">
+        <div className="d-flex flex-column">
+          <h4 className="second-font fs-1">Hosted Venues</h4>
+          {venueManager ? (
+            <>
+              <ul className="third-font fs-4">
+                {venues.length > 0 ? (
+                  venues.map((venue) => (
+                    <VenueCard key={venue.id} venue={venue} />
+                  ))
+                ) : (
+                  <p className="third-font fs-4">No venues</p>
+                )}
+              </ul>
+              <MyButton label="New Venue" onClick={handleShowAddVenue} />
+              <AddVenueModal
+                show={showAddVenueModal}
+                handleClose={handleCloseAddVenue}
+                user={user}
+              />
+            </>
+          ) : (
+            <>
+              <MyButton
+                label="BECOME VENUE MANAGER"
+                onClick={handleShowVenueManager}
+              />
+
+              <VenueManagerModal
+                show={showVenueManagerModal}
+                handleClose={handleCloseVenueManager}
+                user={user}
+              />
+            </>
+          )}
+        </div>
+        <div className="d-flex flex-column">
+          <h4 className="second-font fs-1">Upcoming Bookings</h4>
+          <ul className="third-font fs-4">
+            {bookings.length > 0 ? (
+              bookings.map((booking, index) => (
+                <VenueCard
+                  key={`${booking.venue.id}-${booking.id}-${index}`} // Use a combination to ensure uniqueness
+                  venue={booking.venue}
+                />
+              ))
+            ) : (
+              <p className="third-font fs-4">No bookings</p>
+            )}
+          </ul>
+        </div>
       </div>
     </>
   );
