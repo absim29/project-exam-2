5865ac8148f71de6fe4eed22f1af97489319ef19
absim29 <simon.bianca29@gmail.com>
2024-09-23 15:13:01 +0200

    Added framer motion animation

--- project-exam-2/src/components/Layout/index.jsx
+++ project-exam-2/src/components/Layout/index.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
+import { motion } from "framer-motion";
 
 function Layout({ children }) {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main className="flex-fill d-flex flex-column flex-wrap align-items-center">
-        {children}
-      </main>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <main className="d-flex flex-column align-items-center min-vh-100">
+          {children}
+        </main>
+      </motion.div>
       <Footer />
     </div>
   );
