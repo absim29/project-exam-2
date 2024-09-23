587dec0c73b4bca1837e45326a8c01aca4eec050
absim29 <simon.bianca29@gmail.com>
2024-09-23 15:12:11 +0200

    Update App.jsx

--- project-exam-2/src/App.jsx
+++ project-exam-2/src/App.jsx
@@ -1,39 +1,42 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Venues from "./pages/Venues";
 import SingleVenue from "./pages/SingleVenue";
 import ProfilePage from "./pages/ProfilePage";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <Routes>
-        {/* The LandingPage is displayed when the user is at the root URL */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/venues" element={<Venues />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/venues/:id" element={<SingleVenue />} />
-        {/* Page not found handler for unmatched routes */}
-        <Route
-          path="*"
-          element={
-            <>
-              <h1>Page not found</h1>
-              <div className="prodContainer">
-                <p>
-                  We're sorry, but the page you are looking for doesn't exist.
-                </p>
-                <p>
-                  It might have been moved or deleted, or perhaps the URL is
-                  incorrect.
-                </p>
-              </div>
-            </>
-          }
-        />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/venues" element={<Venues />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/venues/:id" element={<SingleVenue />} />
+          <Route
+            path="*"
+            element={
+              <>
+                <h1>Page not found</h1>
+                <div className="prodContainer">
+                  <p>
+                    We're sorry, but the page you are looking for doesn't exist.
+                  </p>
+                  <p>
+                    It might have been moved or deleted, or perhaps the URL is
+                    incorrect.
+                  </p>
+                </div>
+              </>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
