3083dc6a5f81b128e3a3f946a614c40a28065549
absim29 <simon.bianca29@gmail.com>
2024-09-23 15:13:57 +0200

    Update index.jsx

--- project-exam-2/src/pages/LandingPage/index.jsx
+++ project-exam-2/src/pages/LandingPage/index.jsx
@@ -4,6 +4,7 @@ import MyButton from "../../components/Button";
 import Logo from "../../components/Logo";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 
 function LandingPage() {
   return (
@@ -16,12 +17,22 @@ function LandingPage() {
       <BackgroundVideo />
       <Logo />
       <div className="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-center align-items-center">
-        <h1 className="text-uppercase first-font">Escape the ordinary</h1>
+        <motion.div
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -100, opacity: 0 }}
+          transition={{ duration: 0.6, ease: "easeInOut" }}
+        >
+          <h1 className="text-uppercase first-font">Escape the ordinary</h1>
+        </motion.div>
         <h2 className="second-font">Your Next Adventure Starts Here</h2>
         <Link to="/venues">
           <MyButton label="Explore" />
         </Link>
-        <p className="mt-3">All rights reserved. Copyright 2024 - HOLIDAZE</p>
+        <p className="mt-2 d-flex">
+          <CopyrightIcon />
+          2024 HOLIDAZE | All rights reserved
+        </p>
       </div>
     </motion.div>
   );
