fb3713a78ae1f399df16b973e3a73c242dfd7be2
absim29 <simon.bianca29@gmail.com>
2024-09-24 21:37:49 +0200

    Update index.jsx
    Show user image as logo when logged in

--- project-exam-2/src/components/Header/index.jsx
+++ project-exam-2/src/components/Header/index.jsx
@@ -5,17 +5,21 @@ import { Navbar, NavDropdown } from "react-bootstrap";
 import useSignOut from "../../hooks/useSignOut";
 import LoginModal from "../Modals/LoginModal";
 import RegistrationModal from "../Modals/RegistrationModal";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import UserImage from "../../assets/user-image.jpg";
 
 function Header() {
   const headerBackground = {
     backgroundColor: "#80A3AB",
     paddingBottom: "10px",
     paddingTop: "0",
+    height: "65px",
   };
 
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showRegistrationModal, setShowRegistrationModal] = useState(false);
   const [username, setUsername] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState(null);
 
   // Handlers for Login Modal
   const handleShowLogin = () => setShowLoginModal(true);
@@ -25,55 +29,90 @@ function Header() {
   const handleShowRegistration = () => setShowRegistrationModal(true);
   const handleCloseRegistration = () => setShowRegistrationModal(false);
 
+  // New handler to open Login Modal after registration
+  const handleOpenLogin = () => {
+    setShowRegistrationModal(false); // Close the registration modal
+    setShowLoginModal(true); // Open the login modal
+  };
+
   useEffect(() => {
-    // Retrieve the username from local storage
+    const storedAvatarUrl = localStorage.getItem("avatarUrl");
     const storedUsername = localStorage.getItem("username");
-    setUsername(storedUsername);
+    setAvatarUrl(storedAvatarUrl); // Updates avatarUrl state
+    setUsername(storedUsername); // Updates username state
   }, []);
 
   const signOut = useSignOut();
 
   return (
     <>
-      <Navbar sticky="top" className="p-0 first-font">
+      <Navbar sticky="top" className="p-0 first-font shadow-sm">
         <div style={headerBackground} className="w-100">
           <Logo />
-          <ul className="d-flex justify-content-end gap-3 pt-3 px-2">
+          <ul className="d-flex justify-content-end align-items-center gap-3 pt-3 px-2">
             <NavLink to="/venues" className="text-decoration-none text-black">
               Venues
             </NavLink>{" "}
-            {username ? (
-              <NavDropdown title={username} menuVariant="light" drop="start">
-                <NavDropdown.Item as={NavLink} to="/profile">
-                  My Profile
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/bookings">
-                  My Bookings
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/favourites">
-                  Favourites
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/manage-bookings">
-                  Manage Bookings
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} onClick={signOut}>
-                  Sign Out
-                </NavDropdown.Item>
-              </NavDropdown>
+            {avatarUrl ? (
+              <>
+                <NavDropdown
+                  menuVariant="light"
+                  drop="down"
+                  title={
+                    <span>
+                      <img
+                        src={avatarUrl || UserImage}
+                        alt={username || "User"}
+                        style={{
+                          width: "30px",
+                          height: "30px",
+                          objectFit: "cover",
+                          borderRadius: "50%",
+                        }}
+                      />
+                    </span>
+                  }
+                >
+                  <NavDropdown.Item as={NavLink} to="/profile">
+                    My Profile
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item as={NavLink} onClick={signOut}>
+                    Sign Out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
             ) : (
               <>
-                <NavLink
-                  onClick={handleShowLogin}
-                  className="text-decoration-none text-black"
+                <NavDropdown
+                  menuVariant="light"
+                  drop="down"
+                  title={
+                    <span>
+                      <AccountCircleIcon
+                        style={{
+                          width: "30px",
+                          height: "30px",
+                        }}
+                      />{" "}
+                      {/* Wrap with span */}
+                    </span>
+                  }
                 >
-                  Login
-                </NavLink>
-                <NavLink
-                  onClick={handleShowRegistration}
-                  className="text-decoration-none text-black"
-                >
-                  Register
-                </NavLink>
+                  <NavDropdown.Item
+                    onClick={handleShowLogin}
+                    className="text-decoration-none text-black"
+                  >
+                    Login
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item
+                    onClick={handleShowRegistration}
+                    className="text-decoration-none text-black"
+                  >
+                    Register
+                  </NavDropdown.Item>
+                </NavDropdown>
               </>
             )}
           </ul>
@@ -83,6 +122,7 @@ function Header() {
       <RegistrationModal
         show={showRegistrationModal}
         handleClose={handleCloseRegistration}
+        handleOpenLogin={handleOpenLogin}
       />
     </>
   );
