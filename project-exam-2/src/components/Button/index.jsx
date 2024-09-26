a3056d62b0635a2cbf3af56e58b8138329915e39
absim29 <simon.bianca29@gmail.com>
2024-09-26 16:28:51 +0200

    Update index.jsx

--- project-exam-2/src/components/Button/index.jsx
+++ project-exam-2/src/components/Button/index.jsx
@@ -6,8 +6,8 @@ function MyButton({ label, onClick, disabled, type }) {
     <Button
       variant="outline-dark"
       size="lg"
-      className="px-5 custom-color rounded-pill shadow-sm"
-      style={{ minWidth: "150px", maxWidth: "200px" }}
+      className="px-4 custom-color rounded-pill shadow-sm"
+      style={{ minWidth: "150px" }}
       onClick={onClick}
       disabled={disabled}
       type={type}
